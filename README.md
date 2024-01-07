88e947457592b78e3d732b8a2a328f54020c17e6
sun_xingdi <sun_xingdi@163.com>
2024-01-07 11:23:42 +0800

    提交签名与多签原理相关代码

--- README.md
+++ README.md
@@ -1,5 +1,67 @@
-# signature-and-multisig-demo
-签名与多签原理学习
+# 签名与多签原理学习
+
+### ECDSA签名原理
+
+签名：消息 + 私钥 + 随机数 —> 签名
+
+验签：消息 + 签名 —> 公钥，对比公钥和签名者地址是否一致
+
+签名分为交易签名和消息签名两种类型，原理类似，本次主要介绍消息签名。
+
+#### 交易签名：
+
+签名过程：构造交易->RLP编码->哈希运算->私钥签名->打包签名->RLP编码
+
+- 构造交易：交易结构为[chainId, nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data]。
+- RLP编码：网络传输中json的序列化/反序列化方式不同，为保证交易数据传输后的编码一致性，使用RLP编码序列化方式。
+- 哈希运算：使用Keccak256，输出bytes32哈希值。
+- 私钥签名：使用ECDSA椭圆曲线签名算法进行私钥签名，输出(v、r、s)签名值。
+- 打包签名：把交易和签名打包为[nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data, v, r, s]，chainId已经被编码到v参数中，从交易数据中删除。
+- RLP编码：对打包后的数据再次进行RLP编码。
+
+验证过程：RLP解码->验证签名->对比公钥
+
+- RLP解码：解码后的数据[nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data, v, r, s]
+- 验证签名：对消息[nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, to, value, data]和签名[v, r, s]恢复出公钥，即签名者地址。
+- 对比公钥：对比签名者与签名者公钥推导的地址是否一致。
+
+安全问题：
+- 增加chainId字段，防止跨链重放攻击。（EIP155标准）
+- 增加Nonce字段，防止双花攻击。
+
+交易类型：
+- 单纯转账交易：to为目标地址，data为空。
+- 部署合约交易：to为空，data合约创建字节码。
+- 调用合约交易：to为合约地址，data为函数选择器+参数。
+  
+#### 消息签名：
+
+签名过程：原始消息->打包和哈希->以太坊签名->私钥签名
+
+- 原始消息：任意待签名消息
+- 打包和哈希：对消息进行打包，再使用Keccak256，输出bytes32哈希值。
+- 以太坊签名：添加以太坊消息前缀和再次哈希，输出bytes32哈希值。
+- 私钥签名：使用ECDSA椭圆曲线签名算法进行私钥签名，输出(v、r、s)签名值。
+
+验证过程：原始消息->打包和哈希->以太坊签名->验证签名->对比公钥
+
+- 验证签名：使用消息和签名恢复出公钥。
+- 对比公钥：对比签名者与签名者公钥推导的地址是否一致。
+
+### 签名详细步骤
+
+原始消息：
+```
+let _account = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4';
+let _tokenId = 0;
+```
+
+第1步：进行消息打包和哈希（bytes32）。一般消息比较大，直接对消息进行签名需要消耗更多的gas，对消息的哈希值进行签名可以节省gas。
+```
+let messageHash = ethers.solidityPackedKeccak256(['address', 'uint256'], [_account, _tokenId])
+```
+
+第2步：添加以太坊消息前缀和再次哈希（bytes32）。对上一步的数据添加"\x19Ethereum Signed Message:\n"和字节长度前缀，然后再次进行哈希运算。为了防止签名是可执行的交易。
 
 
 
@@ -15,6 +77,8 @@ https://mirror.xyz/wtfacademy.eth/pVjNv3xzVoOB1AtsXNsZ01b6FZy-iVbdNZsv0qVQ7Qo
 
 https://docs.ethers.org/v6/
 https://docs.ethers.org/v5/
+
+工具：https://app.mycrypto.com/
 
 TODO:
 1、整体签名和验签原理
--- scripts/Signature.ts
+++ scripts/Signature.ts
@@ -2,65 +2,69 @@ import { ethers } from "hardhat";
 
 console.log("\n", "ethers version: ", ethers.version);
 
-// hardhat本地网络内置账户私钥，0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+// hardhat本地网络内置账户：0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'; 
 
 async function main() {
 
-  const SignatureContract = await ethers.getContractFactory("Signature");
-
   console.log("\n", "部署合约...");
+  const SignatureContract = await ethers.getContractFactory("Signature");
   const SigContract = await SignatureContract.deploy();
   console.log("合约地址:", SigContract.target);
 
-  console.log("\n", "设置过滤器，监听事件...")
-  const filter = SigContract.filters.recoverSignerEvent();
-  // SigContract.on(filter, (eventArgs) => {
-  SigContract.once(filter, (eventArgs) => {
-    console.log('Event:', eventArgs.args);
-  });
-
-  console.log("\n", "获取账户...")
-  const [fromSigner, toSigner] = await ethers.getSigners();
+  // console.log("\n", "设置过滤器，监听事件...")
+  // const filter = SigContract.filters.recoverSignerEvent();
+  // // SigContract.on(filter, (eventArgs) => { //实时监听
+  // SigContract.once(filter, (eventArgs) => {  //只监听一次
+  //   console.log('Event:', eventArgs.args);
+  // });
 
   console.log("###############################################################")
-
+  console.log("\n", "处理消息...")
   let _account = '0x5B38Da6a701c568545dCfcB03FcB875f56beddC4';
   let _tokenId = 0;
-  console.log("\n", "原始消息:          ", ['address', 'uint256'], [_account, _tokenId]);
+  console.log("\n", "原始消息:      ", ['address', 'uint256'], [_account, _tokenId]);
 
   let messageHash = ethers.solidityPackedKeccak256(['address', 'uint256'], [_account, _tokenId])
-  console.log("\n", "消息哈希:          ", messageHash);
+  console.log("\n", "打包和哈希消息: ", messageHash);
 
   let ethSignedmessageHash = ethers.solidityPackedKeccak256(['string', 'bytes32'], ["\x19Ethereum Signed Message:\n32", messageHash])
-  console.log("\n", "以太坊签名消息哈希: ", ethSignedmessageHash);
+  console.log("\n", "以太坊签名消息: ", ethSignedmessageHash);
 
-  //#################################################################################
+  console.log("###############################################################")
+  console.log("\n", "签名方法1：ethers.signingKey")
   let signingKey = new ethers.SigningKey(privateKey);
+  console.log("\n", "signingKey地址: ", signingKey.address)
 
   let ethSignedmessageHashSigned = await signingKey.sign(ethSignedmessageHash);
-  console.log("\n", "SigningKey签名:  ", ethSignedmessageHashSigned)
+  console.log("\n", "SigningKey签名: ", ethSignedmessageHashSigned)
 
   let { r, s, v } = ethSignedmessageHashSigned
   console.log("r: ", r, "\ns: ", s, "\nv: ", v)
 
-  //#################################################################################
+  console.log("###############################################################")
+  console.log("\n", "签名方法2：ethers.Wallet")
   const wallet = new ethers.Wallet(privateKey);
-  console.log("\n", "walletAddress: ", wallet.address)
+  console.log("\n", "wallet地址:    ", wallet.address)
 
   let messageHashSigned = await wallet.signMessage(Uint8Array.from(Buffer.from(messageHash.slice(2), 'hex')));
-  console.log("\n", "Wallet签名:           ", messageHashSigned)
-  
-  let messageHashSigned2 = await fromSigner.signMessage(Uint8Array.from(Buffer.from(messageHash.slice(2), 'hex')));
-  console.log("\n", "fromSigner签名:           ", messageHashSigned2)    
+  console.log("\n", "Wallet签名:    ", messageHashSigned)
 
-  //#########################################################################
+  console.log("###############################################################")
+  console.log("\n", "签名方法3：ethers.getSigners")
+  const [getSigners] = await ethers.getSigners();
+  console.log("\n", "getSigners地址: ", getSigners.address)
+  
+  let messageHashSigned2 = await getSigners.signMessage(Uint8Array.from(Buffer.from(messageHash.slice(2), 'hex')));
+  console.log("\n", "getSigners签名: ", messageHashSigned2)    
+
+  console.log("###############################################################")
   console.log("\n", "验证签名恢复公钥...")
   const Signer = await SigContract.recoverSigner(
     ethSignedmessageHash,
     messageHashSigned,
   );
-  console.log("恢复公钥：  ", Signer)
+  console.log("\n", "恢复公钥：  ", Signer)
 
   // const receipt = await Signer.wait();
   // console.log('Transaction Receipt:', receipt); 
